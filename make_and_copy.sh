df9df28dd93cd26cb7d72985e2e0a2f4726e261e
tinglepan <tinglepan@gmail.com>
2020-09-19 22:42:12 +0800

    updated sh

--- make_and_copy.sh
+++ make_and_copy.sh
@@ -1,6 +1,6 @@
 BARREL_PATH=~/barrelfish
 BUILD_DIR=build
-MOUNT_PATH=/mnt
+MOUNT_PATH=/mnt/
 SBIN_DIR=armv8/sbin
 MENU_DIR=hake/menu.lst.armv8_zynqmp
 MENU_NAME=menu.lst
@@ -9,16 +9,11 @@ MENU_NAME=menu.lst
 cd $BUILD_DIR
 make ZynqmpZCU104 -j4
 
-sudo mount -t vboxsf shared_folder $MOUNT_PATH
-
 # cp sbin
 echo "####: waiting for copy.\n"
-sudo rm -r $MOUNT_PATH/sbin
-sudo cp -r $BARREL_PATH/$BUILD_DIR/$SBIN_DIR $MOUNT_PATH/sbin
+sudo rm -r $MOUNT_PATH/$SBIN_DIR
+sudo cp -r $BARREL_PATH/$BUILD_DIR/$SBIN_DIR $MOUNT_PATH/$SBIN_DIR
 sudo cp $BARREL_PATH/$MENU_DIR $MOUNT_PATH/$MENU_NAME
 echo "####: copy done.\n"
-
-#umount sdcard
-sudo umount $MOUNT_PATH
 
 echo "All is OK!"
